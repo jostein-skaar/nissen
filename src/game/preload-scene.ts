ace9137f3f39d48ac9cdf7e0fe7c6785de71c6a8
Jostein Skaar <jostein.skaar@gmail.com>
2021-12-29 09:36:47 +0100

    Need to postfix urls with ?v=version to prevent caching - for now

--- src/game/preload-scene.ts
+++ src/game/preload-scene.ts
@@ -15,34 +15,34 @@ export class PreloadScene extends Phaser.Scene {
   preload() {
     console.log('preload-scene');
 
-    this.load.spritesheet('helt', `/assets/helt-sprite@${fiksForPikselratio(1)}.png`, {
+    this.load.spritesheet('helt', `/assets/helt-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`, {
       frameWidth: fiksForPikselratio(32),
       frameHeight: fiksForPikselratio(40),
       margin: 1,
       spacing: 2,
     });
 
-    this.load.spritesheet('present', `/assets/presents-sprite@${fiksForPikselratio(1)}.png`, {
+    this.load.spritesheet('present', `/assets/presents-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`, {
       frameWidth: fiksForPikselratio(32),
       frameHeight: fiksForPikselratio(32),
       margin: 1,
       spacing: 2,
     });
 
-    this.load.spritesheet('corona', `/assets/korona-sprite@${fiksForPikselratio(1)}.png`, {
+    this.load.spritesheet('corona', `/assets/korona-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`, {
       frameWidth: fiksForPikselratio(32),
       frameHeight: fiksForPikselratio(32),
       margin: 1,
       spacing: 2,
     });
 
-    this.load.image('tiles', `/assets/tiles-sprite@${fiksForPikselratio(1)}.png`);
-    this.load.image('presents', `/assets/presents-sprite@${fiksForPikselratio(1)}.png`);
-    this.load.image('coronas', `/assets/korona-sprite@${fiksForPikselratio(1)}.png`);
+    this.load.image('tiles', `/assets/tiles-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`);
+    this.load.image('presents', `/assets/presents-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`);
+    this.load.image('coronas', `/assets/korona-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`);
 
     if (this.useParallax) {
-      this.load.image('background-mountains', `/assets/background-mountains@1.png`);
-      this.load.image('background-snow', `/assets/background-snow@1.png`);
+      this.load.image('background-mountains', `/assets/background-mountains@1.png?v={VERSJON}`);
+      this.load.image('background-snow', `/assets/background-snow@1.png?v={VERSJON}`);
     }
 
     // this.load.spritesheet('background', `/assets/background-sprite@${fiksForPikselratio(1)}.png`, {
@@ -52,7 +52,7 @@ export class PreloadScene extends Phaser.Scene {
     //   spacing: 2,
     // });
 
-    this.load.spritesheet('snow', `/assets/snow-sprite@${fiksForPikselratio(1)}.png`, {
+    this.load.spritesheet('snow', `/assets/snow-sprite@${fiksForPikselratio(1)}.png?v={VERSJON}`, {
       frameWidth: fiksForPikselratio(16),
       frameHeight: fiksForPikselratio(16),
       margin: 1,
